7986c9d886995ccef42d214643791ce6c6249ab1
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-11-27 15:47:27 +0100

    edit task names by click

--- src/main/resources/nfms/modules/g.js
+++ src/main/resources/nfms/modules/g.js
@@ -305,7 +305,58 @@ define([ "utils", "d3.v3" ], function(utils) {
 
 		var yAxis = d3.svg.axis().scale(yScale).orient("left").tickSize(1);
 		svg.append("g").attr("class", "y axis").transition().call(yAxis);
+		svg.selectAll(".y.axis .tick").on("click", function(d) {
+			var tickSelection = d3.select(this);
+			var input = tickSelection.append("foreignObject")//
+			.attr("width", "300px")//
+			.attr("height", "25px")//
+			.attr("class", "editable")//
+			.append("xhtml:form").append("input").attr("value", function() {
+				// nasty spot to place this call, but here we are sure that the
+				// <input> tag is available
+				// and is handily pointed at by 'this':
+				this.focus();
 
+				return d;
+			}).attr("style", "width: 294px;")
+			// make the form go away when you jump out (form looses focus) or
+			// hit ENTER:
+			.on("blur", function() {
+				getTask(ROOT, d).taskName = input.node().value;
+				// Note to self: frm.remove() will remove the entire <g> group!
+				// Remember the D3 selection logic!
+				tickSelection.select(".editable").remove();
+				updateChart(plan);
+			}).on("keypress", function() {
+				// IE fix
+				if (!d3.event)
+					d3.event = window.event;
+
+				var e = d3.event;
+				if (e.keyCode == 13) {
+					input.node().blur();
+					// if (typeof (e.cancelBubble) !== 'undefined') // IE
+					// e.cancelBubble = true;
+					// if (e.stopPropagation)
+					// e.stopPropagation();
+					// e.preventDefault();
+					//
+					// var txt = inp.node().value;
+					//
+					// d[field] = txt;
+					// el.text(function(d) {
+					// return d[field];
+					// });
+					//
+					// // odd. Should work in Safari, but the debugger crashes
+					// on
+					// // this instead.
+					// // Anyway, it SHOULD be here and it doesn't hurt
+					// otherwise.
+					// p_el.select("foreignObject").remove();
+				}
+			});
+		});
 		var xAxis1 = d3.svg.axis().scale(xScale).orient("bottom").tickFormat(
 				d3.time.format("%d/%m")).tickSize(1).tickPadding(8);
 		svg.append("g").attr("class", "x axis").attr("transform",

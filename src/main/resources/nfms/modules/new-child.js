4034425ce60dd9ce2b5da72d33eb4b343fa9837f
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-12-01 20:54:51 +0100

    INSERT to create a new child

---
+++ src/main/resources/nfms/modules/new-child.js
@@ -0,0 +1,33 @@
+define([ "message-bus", "task-tree", "data-reader", "d3" ], function(bus, taskTree, dataReader) {
+
+	var selectedTaskName;
+
+	bus.listen("keypress", function(e, d3Event) {
+		if (d3Event.keyCode == 45) {
+			var newTaskName = "new task";
+			var parentTask = taskTree.getTask(selectedTaskName);
+			var newTask = {
+				taskName : newTaskName,
+				status : parentTask.status
+			};
+			if (parentTask.getStartDate() != null) {
+				newTask.startDate = parentTask.getStartDate();
+			}
+			if (parentTask.getEndDate() != null) {
+				newTask.endDate = parentTask.getEndDate();
+			}
+			if (!parentTask.hasOwnProperty("tasks")) {
+				parentTask["tasks"] = [];
+			}
+			dataReader.decorateTask(newTask);
+			parentTask.tasks.splice(0, 0, newTask);
+			bus.send("refresh-tree");
+			bus.send("select-task", [ newTaskName ]);
+			bus.send("edit-selected");
+		}
+	});
+
+	bus.listen("selection-update", function(e, newSelectedName) {
+		selectedTaskName = newSelectedName;
+	});
+});

30b2cad6ebf8a4d642509eec386f5011d323c93b
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-12-01 21:41:16 +0100

    ENTER and ESC keys while editing task names

--- src/main/resources/nfms/modules/edit-task-names.js
+++ src/main/resources/nfms/modules/edit-task-names.js
@@ -29,13 +29,15 @@ define([ "message-bus", "task-tree" ], function(bus, taskTree) {
 		// make the form go away when you jump out (form looses focus) or
 		// hit ENTER:
 		.on("blur", function() {
+			var task = taskTree.getTask(selectedTaskName);
 			if (!cancel) {
-				var task = taskTree.getTask(selectedTaskName);
 				task.taskName = input.node().value;
-				// Note to self: frm.remove() will remove the entire <g>
-				// group!
-				// Remember the D3 selection logic!
-				tickSelection.select(".editable").remove();
+			}
+			// Note to self: frm.remove() will remove the entire <g>
+			// group!
+			// Remember the D3 selection logic!
+			tickSelection.select(".editable").remove();
+			if (!cancel) {
 				bus.send("refresh-tree");
 				bus.send("select-task", [ task.taskName ]);
 			}
@@ -47,11 +49,11 @@ define([ "message-bus", "task-tree" ], function(bus, taskTree) {
 			var e = d3.event;
 			if (e.keyCode == 13) {
 				input.node().blur();
-				// if (typeof (e.cancelBubble) !== 'undefined') // IE
-				// e.cancelBubble = true;
-				// if (e.stopPropagation)
-				// e.stopPropagation();
-				// e.preventDefault();
+				if (typeof (e.cancelBubble) !== 'undefined') // IE
+					e.cancelBubble = true;
+				if (e.stopPropagation)
+					e.stopPropagation();
+				e.preventDefault();
 				//
 				// var txt = inp.node().value;
 				//

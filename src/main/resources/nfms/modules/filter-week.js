68dac6836123e7e2876cd79e85f793a363809ceb
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-12-02 21:25:43 +0100

    filter week

---
+++ src/main/resources/nfms/modules/filter-week.js
@@ -0,0 +1,22 @@
+define([ "message-bus", "utils", "d3" ], function(bus, utils) {
+
+	var weekStart = new Date(utils.today.getTime());
+	weekStart.setDate(weekStart.getDate() - weekStart.getDay() + 1);
+	var weekEnd = new Date(weekStart.getTime() + utils.DAY_MILLIS * 6);
+
+	console.log(weekStart);
+	console.log(weekEnd);
+
+	var FILTER_WEEK = function(task) {
+		return (task.getEndDate() >= weekStart && task.getEndDate() <= weekEnd)
+				|| (task.getStartDate() >= weekStart && task.getStartDate() <= weekEnd)
+				|| (task.getStartDate() < weekStart && task.getEndDate() > weekEnd);
+	};
+
+	d3.select("body").append("input")//
+	.attr("type", "button")//
+	.attr("value", "Mostrar semana").on("click", function() {
+		bus.send("filter", [ FILTER_WEEK ]);
+	});
+
+});

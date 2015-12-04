b90d89817741c56780bff4fd2f205337f0d3ec4d
Fernando González Cortés <fernando.gonzalez@geomati.co>
2015-12-04 08:33:32 +0100

    show groups in today and week filter

--- src/main/resources/nfms/modules/filter-today.js
+++ src/main/resources/nfms/modules/filter-today.js
@@ -1,6 +1,7 @@
 define([ "message-bus", "utils", "d3" ], function(bus, utils) {
 	var FILTER_TODAY = function(task) {
-		return task.getStartDate() <= utils.today && task.getEndDate() >= utils.today;
+		return task.hasOwnProperty("tasks")
+				|| (task.getStartDate() <= utils.today && task.getEndDate() >= utils.today);
 	};
 
 	d3.select("body").append("input")//
